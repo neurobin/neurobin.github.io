9cd5350ab3ee31e9e0e9af0bc8bc4a05f2834160
mdjahidulhamid <jahidulhamid@yahoo.com>
2015-06-01 16:01:29 +0600

    uedit first incomplete release

--- uedit/scripts/uedit.js
+++ uedit/scripts/uedit.js
@@ -6,31 +6,32 @@ var tagStart="",tagEnd="";
 sLnk="";
 localStoragePrefix="neurobin-uedit-";
 projectName="uedit";
-
+autoSaveTimeout=5000;
+contextMenu="context-menu";
 
 var jsonDefault = '{"html":[' +
-'{"id":"","start":"","end":"","title":"Define it manually","class":"editor-button","innerhtml":"General"},' +
-'{"id":"","start":"<!-- ","end":" -->","title":"Comment","class":"editor-button","innerhtml":"Comment"  },' +
-'{"id":"","start":"<p>","end":"</p>","title":"Paragraph","class":"editor-button","innerhtml":"P"  },' +
-'{"id":"","start":"<a ","end":"</a>","title":"Hyperlink","class":"editor-button","innerhtml":"a href","href":""  },' +
-'{"id":"","start":"<ol>","end":"</ol>","title":"Ordered List","class":"editor-button","innerhtml":"ol"  },' +
-'{"id":"","start":"<ul>","end":"</ul>","title":"Unordered List","class":"editor-button","innerhtml":"ul"  },' +
-'{"id":"","start":"<li>","end":"</li>","title":"List Item","class":"editor-button","innerhtml":"li"  },' +
-'{"id":"","start":"<quote>","end":"</quote>","title":"Quote","class":"editor-button","innerhtml":"q"  },' +
-'{"id":"","start":"<blockquote>","end":"</blockquote>","title":"Block Quote","class":"editor-button","innerhtml":"bq"  },' +
-'{"id":"","start":"<pre>","end":"</pre>","title":"pre","class":"editor-button","innerhtml":"pre"  },' +
-'{"id":"","start":"<code>","end":"</code>","title":"Inline Code","class":"editor-button","innerhtml":"code"  },' +
-'{"id":"","start":"<pre><code>","end":"</code></pre>","title":"Pre-formatted Code","class":"editor-button","innerhtml":"pre/code"  },' +
-'{"id":"","start":"<img ","end":" />","title":"Image","class":"editor-button","innerhtml":"img","src":""  },' +
-'{"id":"","start":"<span>","end":"</span>","title":"Span","class":"editor-button","innerhtml":"span"  },' +
-'{"id":"","start":"<div>","end":"</div>","title":"Div","class":"editor-button","innerhtml":"div"  },' +
-'{"id":"","start":"<sup>","end":"</sup>","title":"Superscript","class":"editor-button","innerhtml":"sup"  },' +
-'{"id":"","start":"<sub>","end":"</sub>","title":"Subscript","class":"editor-button","innerhtml":"sub"  },' +
-'{"id":"","start":"<kbd>","end":"</kbd>","title":"Keboad key","class":"editor-button","innerhtml":"kbd"  },' +
-'{"id":"","start":"","end":"<hr>","title":"Horizontal Rule","class":"editor-button","innerhtml":"hr"  },' +
-'{"id":"","start":"","end":"<br>","title":"New Line","class":"editor-button","innerhtml":"br"  },' +
-'{"id":"","start":"<var>","end":"</var>","title":"Variable","class":"editor-button","innerhtml":"var"  },' +
-'{"id":"","start":"<del>","end":"</del>","title":"Deleted text","class":"editor-button","innerhtml":"<del>del</del>"  }]}';
+'{"id":"","start":"","end":"","title":"Define it manually","class":"editor-button","innerhtml":"General","type":"textarea"  },' +
+'{"id":"","start":"<!-- ","end":" -->","title":"Comment","class":"editor-button","innerhtml":"Comment","type":"input"  },' +
+'{"id":"","start":"<p>","end":"</p>","title":"Paragraph","class":"editor-button","innerhtml":"P","type":"input"  },' +
+'{"id":"","start":"<a ","end":"</a>","title":"Hyperlink","class":"editor-button","innerhtml":"a href","href":"","type":"input"  },' +
+'{"id":"","start":"<ol>","end":"</ol>","title":"Ordered List","class":"editor-button","innerhtml":"ol","type":"input"  },' +
+'{"id":"","start":"<ul>","end":"</ul>","title":"Unordered List","class":"editor-button","innerhtml":"ul","type":"input"  },' +
+'{"id":"","start":"<li>","end":"</li>","title":"List Item","class":"editor-button","innerhtml":"li","type":"input"  },' +
+'{"id":"","start":"<quote>","end":"</quote>","title":"Quote","class":"editor-button","innerhtml":"q","type":"input"  },' +
+'{"id":"","start":"<blockquote>","end":"</blockquote>","title":"Block Quote","class":"editor-button","innerhtml":"bq","type":"input"  },' +
+'{"id":"","start":"<pre>","end":"</pre>","title":"pre","class":"editor-button","innerhtml":"pre","type":"input"  },' +
+'{"id":"","start":"<code>","end":"</code>","title":"Inline Code","class":"editor-button","innerhtml":"code","type":"input"  },' +
+'{"id":"","start":"<pre><code>","end":"</code></pre>","title":"Pre-formatted Code","class":"editor-button","innerhtml":"pre/code","type":"input"  },' +
+'{"id":"","start":"<img ","end":" />","title":"Image","class":"editor-button","innerhtml":"img","src":"","type":"input"  },' +
+'{"id":"","start":"<span>","end":"</span>","title":"Span","class":"editor-button","innerhtml":"span","type":"input"  },' +
+'{"id":"","start":"<div>","end":"</div>","title":"Div","class":"editor-button","innerhtml":"div","type":"input"  },' +
+'{"id":"","start":"<sup>","end":"</sup>","title":"Superscript","class":"editor-button","innerhtml":"sup","type":"input"  },' +
+'{"id":"","start":"<sub>","end":"</sub>","title":"Subscript","class":"editor-button","innerhtml":"sub","type":"input"  },' +
+'{"id":"","start":"<kbd>","end":"</kbd>","title":"Keboad key","class":"editor-button","innerhtml":"kbd","type":"input"  },' +
+'{"id":"","start":"","end":"<hr>","title":"Horizontal Rule","class":"editor-button","innerhtml":"hr","type":"input"  },' +
+'{"id":"","start":"","end":"<br>","title":"New Line","class":"editor-button","innerhtml":"br","type":"input"  },' +
+'{"id":"","start":"<var>","end":"</var>","title":"Variable","class":"editor-button","innerhtml":"var","type":"input"  },' +
+'{"id":"","start":"<del>","end":"</del>","title":"Deleted text","class":"editor-button","innerhtml":"<del>del</del>","type":"input"  }]}';
 
 json=jsonDefault;
 
@@ -94,7 +95,7 @@ else {alert("Warning: Local Storage isn't supported..");}
 
 
 function getInputDialogFieldsFromStorage() {
-var inputdialogfieldes=document.getElementById('uedit-add-button-dialog').getElementsByTagName('input');
+var inputdialogfieldes=document.getElementsByName('input-dialog-input-field');
 for (var i=0;i<inputdialogfieldes.length;i++) {
 //if (localStorage.getItem("neurobin-uedit-"+inputdialogfieldes[i].id!="")) {
 inputdialogfieldes[i].value=localStorage.getItem("neurobin-uedit-"+inputdialogfieldes[i].id);}//}
@@ -102,7 +103,7 @@ inputdialogfieldes[i].value=localStorage.getItem("neurobin-uedit-"+inputdialogfi
 
 
 function getToolBar1InputFields() {
-var toolBar1inputfields=document.getElementById('toolBar1').getElementsByTagName('input');
+var toolBar1inputfields=document.getElementsByName('toolBar1-input-field');
 
 for (var i=0;i<toolBar1inputfields.length;i++) {
 //if (localStorage.getItem("neurobin-uedit-"+toolBar1inputfields[i].id)!="") {
@@ -122,13 +123,17 @@ getToolBar1InputFields();
 
 json=getJSONString();
 
-
+document.getElementById('save-as-path-input-field').value=localStorage.getItem('neurobin-uedit-save-as-filename');
 
 }
-
+//////////////////////////////Document is loaded inside the below statement
 document.addEventListener('DOMContentLoaded', function () {
 var fun=getFromStorage();
 var fun1=setMainContentFromStorage();
+document.body.onclick=function(){
+hideContextMenu();
+
+};
 $("body").css("overflow", "hidden");
     }, false);
     
@@ -142,7 +147,7 @@ else {alert("Warning: Local Storage isn't supported..");}
 
     
 function fillStorageFromInputDialogFields() {
-var inputdialogfieldes=document.getElementById('uedit-add-button-dialog').getElementsByTagName('input');
+var inputdialogfieldes=document.getElementsByName('input-dialog-input-field');
     for (var i=0;i<inputdialogfieldes.length;i++) {
 localStorage.setItem("neurobin-uedit-"+inputdialogfieldes[i].id,inputdialogfieldes[i].value);}
 
@@ -150,14 +155,14 @@ localStorage.setItem("neurobin-uedit-"+inputdialogfieldes[i].id,inputdialogfield
 
 
 function fillStorageFromToolBar1InputFields(){
-    var toolBar1inputfields=document.getElementById('toolBar1').getElementsByTagName('input');
+    var toolBar1inputfields=document.getElementsByName('toolBar1-input-field');
 
     for (var i=0;i<toolBar1inputfields.length;i++) {
 localStorage.setItem("neurobin-uedit-"+toolBar1inputfields[i].id,toolBar1inputfields[i].value);}
 
 }
 
-function filStorageWithMainContent(){
+function fillStorageWithMainContent(){
 fillStorageById("editor-main-content",editor.getSession().getValue());
 }
 function setMainContentFromStorage(){
@@ -183,13 +188,14 @@ fillStorageFromToolBar1InputFields();
 
 
 localStorage.setItem('neurobin-uedit-json',json);
-filStorageWithMainContent();
+fillStorageWithMainContent();
     // Retrieve
     //document.getElementById("result").innerHTML = localStorage.getItem("lastname");
 } else {
     alert("Warning: Local Storage isn't supported..");
 }
 
+localStorage.setItem('neurobin-uedit-save-as-filename',document.getElementById('save-as-path-input-field').value);
 
 
 }
@@ -215,13 +221,13 @@ filStorageWithMainContent();
 
 
 
-function getSelectedText(){var text="";
+/*function getSelectedText(){var text="";
 if (window.getSelection) {text=window.getSelection();}
 else{text=document.selection.createRange().text;}
 return text;
-}
+}*/
 
-function getSelectionHtml() {
+/*function getSelectionHtml() {
     var html = "";
     if (typeof window.getSelection != "undefined") {
         var sel = window.getSelection();
@@ -238,10 +244,10 @@ function getSelectionHtml() {
         }
     }
     return html;
-}
+}*/
 
 
-function replaceSelectedText(replacementText) {
+/*function replaceSelectedText(replacementText) {
     var sel, range;
     if (window.getSelection) {
         sel = window.getSelection();
@@ -254,7 +260,7 @@ function replaceSelectedText(replacementText) {
         range = document.selection.createRange();
         range.text = replacementText;
     }
-}
+}*/
 
 
 
@@ -325,8 +331,10 @@ function createButtonFromAnyJSON(jsonstring,parentId,lang,classname){
     for(var i=0;i<array.length;i++){
     var element = document.createElement("BUTTON");
     var brElement = document.createElement("BR");
-    var startInput = document.createElement("input");
-    var endInput = document.createElement("input");
+    var type="input";
+    if (array[i].type=="textarea") {type="textarea";}
+    var startInput = document.createElement(type);
+    var endInput = document.createElement(type);
     //Assign different attributes to the element. 
     element.innerHTML=array[i].innerhtml;
     if(array[i].id==""||array[i].id==null){
@@ -340,27 +348,61 @@ function createButtonFromAnyJSON(jsonstring,parentId,lang,classname){
     element.class=array[i].class+" "+classname;
     //alert('fds');
     element.onclick=function(){wrapSelectedText("html","editor-container",this.id);};
+    /////show context menu on right click
+    if (element.addEventListener) {
+        element.addEventListener('contextmenu', function(e) {
+            showContextMenu(this);
+            e.preventDefault();
+        }, false);
+    } else {
+        element.attachEvent('oncontextmenu', function() {
+           showContextMenu(this);
+            window.event.returnValue = false;
+        });
+    }
+    
+    
+    
 //create input fields
 startInput.value=array[i].start;
-startInput.type="text";
+if(array[i].type!="textarea"){startInput.type="text";}
 startInput.id=element.id+"-start";
 startInput.placeholder="start";
-startInput.title="This string will be inserted at the start of selection";
-    
+if(array[i].type="textarea"){startInput.style.resize="none";}
+startInput.name="toolBar1-input-field";
+//if(type!="textarea"){startInput.style.overflowY="none";}
+startInput.onmouseover=function(){setTitleAsValueOfThisElement(this,"This will be inserted at the start of selection");}
+
+
 endInput.value=array[i].end;
-endInput.type="text";
+if(array[i].type!="textarea"){endInput.type="text";}
 endInput.id=element.id+"-end";
 endInput.placeholder="end";
-endInput.title="This string will be inserted at the end of selection";
-    
-    
+if(array[i].type="textarea"){endInput.style.resize="none";}
+endInput.name="toolBar1-input-field";
+//if(type!="textarea"){endInput.style.overflowY="none";}
+endInput.onmouseover=function(){setTitleAsValueOfThisElement(this,"This will be inserted at the end of selection");}
     
     var foo = document.getElementById(parentId);
-    //Append the element in page (in span).  
-    foo.appendChild(element);
-    foo.appendChild(startInput);
-    foo.appendChild(endInput);
-    foo.appendChild(brElement);
+    //Append the element in page (in span).
+var tr=document.createElement("tr");  
+var td1=document.createElement("td");
+var td2=document.createElement("td");
+var td3=document.createElement("td");
+td1.style.width="30%";
+td2.style.width="35%";
+td2.style.width="35%";
+td2.style.overflow="none";
+td3.style.overflow="none";
+td1.appendChild(element);
+td2.appendChild(startInput);
+td3.appendChild(endInput);
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+    
+    
+    foo.appendChild(tr);
 }
 
 }
@@ -375,9 +417,9 @@ var fun=getFromStorage();
 }
 
 
-function createNewButton(parentId,lang,start,end,title,classname,innerhtml,position){
+function createNewButton(parentId,lang,start,end,title,classname,innerhtml,type,position){
 
-insertIntoJSON(lang,start,end,title,classname,innerhtml,position);
+insertIntoJSON(lang,start,end,title,classname,innerhtml,type,position);
 
 document.getElementById(parentId).innerHTML="";
 createButtonFromJSON(parentId,lang,classname);
@@ -388,7 +430,7 @@ createButtonFromJSON(parentId,lang,classname);
 }
 
 function validateForm(formId,parentId){
-var inputfields=document.getElementById(formId).getElementsByTagName("input");
+var inputfields=document.getElementsByName("input-dialog-input-field");
 for(var i=0;i<inputfields.length;i++){
 if (inputfields[i].checkValidity()==false) {
 alert("Error: "+inputfields[i].title+" correctly");
@@ -410,10 +452,12 @@ var end=document.getElementById(formId+"-end").value;
 var title=document.getElementById(formId+"-title").value;
 var classname=document.getElementById(formId+"-class").value;
 var innerhtml=document.getElementById(formId+"-innerhtml").value;
+var type=document.getElementById(formId+"-type").value;
 var position=document.getElementById(formId+"-position").value;
 
+if (type!="textarea") {type="input";}
 
- createNewButton(parentId,lang,start,end,title,classname,innerhtml,position);
+ createNewButton(parentId,lang,start,end,title,classname,innerhtml,type,position);
  
 
 
@@ -421,7 +465,7 @@ var position=document.getElementById(formId+"-position").value;
 
 function itemGone(itemId){
 document.getElementById(itemId).style.transitionDuration="1s";
-document.getElementById(itemId).style.right="-40%";
+document.getElementById(itemId).style.right="-100%";
 }
 
 
@@ -431,7 +475,7 @@ document.getElementById(formId).style.right="0";
 }
 
 
-function insertIntoJSON(lang,start,end,title,classname,innerhtml,position){
+function insertIntoJSON(lang,start,end,title,classname,innerhtml,type,position){
 
 json=getJSONString();
 var obj=JSON.parse(json);
@@ -441,13 +485,14 @@ var result = position.match(patt);
 if (position<0) {position=0;}
 else if (result==null||result==""||position>array.length) {position=array.length;}
 
-newarrayitem={"id":"","start":"","end":"","title":"","class":"editor-button","innerhtml":"" };
+var newarrayitem={"id":"","start":"","end":"","title":"","class":"editor-button","innerhtml":"","type":"" };
 newarrayitem.id=lang+"-btn"+array.length;
 newarrayitem.start=start;
 newarrayitem.end=end;
 newarrayitem.title=title;
 newarrayitem.class=classname;
 newarrayitem.innerhtml=innerhtml;
+newarrayitem.type=type;
 //array.splice(position,0,newarrayitem);
 //array.push(newarrayitem);
 array.splice(position,0,newarrayitem);
@@ -483,7 +528,7 @@ function resetButtonsToDefault(parentId,lang,classname){
         
     }	*/
 	
-var call1=openModalDialog("Attention!!","Are you sure you want to delete all custom buttons?");
+var call1=openModalDialog("Attention!!","Are you sure you want to delete all button customizaion and reset to default ?");
 
 
 }
@@ -527,5 +572,113 @@ var call1=closeModalDialog();
 	
 
 }
+function autoSaveMainContent(){
+	fillStorageWithMainContent();
+	setTimeout(autoSaveMainContent,autoSaveTimeout);
+
+}
+
+function setTitleAsValueOfThisElement(id,defaultTitle) {
+if(id.value!=""&&id.value!=null){id.title=id.value;}else{id.title=defaultTitle;}
+}
+
+function showContextMenu(id){
+	
+var pos=getPosition(id);
+var body = document.body,html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight,html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+    ctxm=document.getElementById(contextMenu); 
+    ctxm.style.left=pos.x+"px";
+    ctxm.style.top=pos.y+"px";
+    ctxm.style.display="block";
+if(pos.y>height-ctxm.scrollHeight){pos.y=pos.y-(pos.y-height+ctxm.scrollHeight)-5;}
+ctxm.style.top=pos.y+"px";
+ctxmItems=ctxm.getElementsByTagName("a");
+for(var i=0;i<ctxmItems.length;i++){
+ctxmItems[i].onclick=function(){
+processInputFromContextMenu(id,this.name);
+}}
+
+}
 
 
+
+function hideContextMenu(){
+    document.getElementById(contextMenu).style.display="none";
+}
+
+
+
+/*function getPosition(e) {
+  var posx = 0;
+  var posy = 0;
+ 
+ if(!e){ var e = window.event;}
+ 
+  if (e.pageX || e.pageY) {
+    posx = e.pageX;
+    posy = e.pageY;
+  } else if (e.clientX || e.clientY) {
+    posx = e.clientX + document.body.scrollLeft +
+                       document.documentElement.scrollLeft;
+    posy = e.clientY + document.body.scrollTop +
+                       document.documentElement.scrollTop;
+  }
+ 
+  return {
+    x: posx,
+    y: posy
+  }
+}*/
+
+function getPosition(element) {
+    var xPosition = 0;
+    var yPosition = 0;
+      
+    while (element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+    return { x: xPosition, y: yPosition };
+}
+
+function processInputFromContextMenu(id,itemName){
+
+if (itemName=="delete") {
+deleteButtonFromArray("toolBar1","html",[id.id]);
+
+
+}
+
+
+}
+
+
+function deleteButtonFromArray(parentId,lang,idarr){
+json=getJSONString();
+var obj=JSON.parse(json);
+array=obj.html;
+for (var j=0;j<idarr.length;j++) {
+for (var i=0;i<array.length;i++) {
+	if (array[i].id==idarr[j]) {array.splice(i,1);}
+
+}}
+
+json=JSON.stringify(obj);
+localStorage.setItem('neurobin-uedit-json',json);
+document.getElementById(parentId).innerHTML="";
+createButtonFromJSON(parentId,lang,"editor-button");
+
+}
+
+
+function saveAsUeditMainContent(){
+	var filename=document.getElementById("save-as-path-input-field").value;
+	if(filename==""||filename==null){filename="uedited-file";}
+var blob = new Blob([editor.session.getValue()], {type: "text/plain;charset=utf-8"});
+saveAs(blob, filename);
+
+}
